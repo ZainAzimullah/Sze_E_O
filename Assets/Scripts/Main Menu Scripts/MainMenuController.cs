7c4509657af35931bd721113fda2a266a745c7ac
Zain's University Laptop <zain.zain51@gmail.com>
2018-09-27 16:33:27 +1200

    Main menu complete - Zain, Brian, Benny

---
+++ Assets/Scripts/Main Menu Scripts/MainMenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void StartGame() {
+		Debug.Log("START BUTTON IS WORKING");
+		SceneManager.LoadScene("Gameplay");
+	}
+
+	public void ResumeGame() {
+
+		Debug.Log("RESUME BUTTON IS WORKING");
+		SceneManager.LoadScene("Gameplay");
+	}
+
+	public void Options() {
+
+		Debug.Log("OPTIONS BUTTON IS WORKING");
+		SceneManager.LoadScene("Options");
+	}
+
+	public void Quit() {
+
+		Debug.Log("QUIT BUTTON IS WORKING");
+		Application.Quit();
+	}
+
+	public void MainMenu() {
+		SceneManager.LoadScene("MainMenu");
+	}
+
+}
