3d39ce27147f6787ff413ff38ca494b6824b3d57
Chen <czha959@aucklanduni.ac.nz>
2018-09-28 17:37:28 +1200

    Put 3d character in the repo

---
+++ Assets/MainCharacter/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //velocity and turning speed of the character
+    public float velocity = 15;
+    public float turnSpeed = 10;
+
+    public GameObject gb;
+    Animator animator;
+
+    Vector2 input;
+    float angle;
+
+    Quaternion targetRotation;
+    Transform cam;
+
+
+
+    // Use this for initialization
+    void Start () {
+        animator = gb.GetComponent<Animator>();
+        cam = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetInput();
+        if(Mathf.Abs(input.x)<1 && Mathf.Abs(input.y) < 1){
+            animator.SetBool("isWalking", false);
+            return;
+        }
+        CalculateDirection();
+        Rotate();
+        Move();
+    }
+
+    //A method to make the character move
+    void Move()
+    {
+        animator.SetBool("isWalking", true);
+        transform.position += transform.forward * velocity * Time.deltaTime;   
+    }
+
+    //a method to get the user input
+    void GetInput()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    //calculate the direction of the character.
+    void CalculateDirection()
+    {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+    }
+
+    //rotate the character
+    void Rotate()
+    {
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation,targetRotation,turnSpeed*Time.deltaTime);
+       
+    }
+}
