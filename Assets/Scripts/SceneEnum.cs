cff69f416c197ae9ba285c5b351076f46cdfb9dc
Chen <czha959@aucklanduni.ac.nz>
2018-09-30 00:15:38 +1200

    Create  scene transition manager for scene transition

---
+++ Assets/Scripts/SceneEnum.cs
@@ -0,0 +1,11 @@
+﻿/**
+ A group of enums representing the name of the scene
+ */
+public enum SceneEnum  {
+    MAIN_MENU,
+    LEVEL1,
+    OPTIONS,
+    ELEVATOR,
+    DIALOGUE,
+    BOOLEAN_GAME
+}
---
+++ Assets/Scripts/SceneTransitionManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionManager : MonoBehaviour {
+
+    private SceneTransitionManager manager;
+
+    public SceneEnum CurrentScene
+    {
+        get; set;
+    }
+
+    private SceneTransitionManager() {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public SceneTransitionManager GetInstance()
+    {
+        if (manager == null)
+        {
+            manager = new SceneTransitionManager();
+        }
+        return manager;
+    }
+
+    public void LoadScene()
+    {
+        string scene = MapScene(CurrentScene);
+        SceneManager.LoadScene(scene);
+    }
+
+    private string MapScene(SceneEnum scene)
+    {
+        string sceneName="";
+        if (scene == SceneEnum.MAIN_MENU)
+        {
+            sceneName = "MainMenu";
+        }
+        if (scene == SceneEnum.OPTIONS)
+        {
+            sceneName = "Options";
+        }
+        if (scene == SceneEnum.LEVEL1)
+        {
+            sceneName = "Level1";
+        }
+        if (scene == SceneEnum.ELEVATOR)
+        {
+            sceneName = "Elevator";
+        }
+        if (scene == SceneEnum.DIALOGUE)
+        {
+            sceneName = "Dialogue";
+        }
+        if (scene == SceneEnum.BOOLEAN_GAME)
+        {
+            sceneName = "BooleanGame";
+        }
+        return sceneName;
+    }
+}
