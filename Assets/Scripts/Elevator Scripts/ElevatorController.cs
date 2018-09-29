d2cf478f935b303526643bc55d53a4461ff707dc
Sze Meeng Tan <stan557@aucklanduni.ac.nz>
2018-09-29 16:58:27 +1200

    linked to level1

---
+++ Assets/Scripts/Elevator Scripts/ElevatorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ElevatorController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void GroundButton() {
+		Debug.Log("START BUTTON IS WORKING");
+		SceneManager.LoadScene("Gameplay");
+	}
+
+    public void LevelOne()
+    {
+        Debug.Log("LEVEL ONE BUTTON IS WORKING");
+        SceneManager.LoadScene("Level1");
+    }
+
+}
