c4abf7328bc8384d544dfed698c444543ebd01a4
Zain's University Laptop <zain.zain51@gmail.com>
2018-09-30 20:14:19 +1300

    fixed compile error

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,29 +1,36 @@
 ﻿using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour {
-
-    public Transform Player;
-    private Vector3 Offset;
-
-    public bool RotateCamera = true;
-    public float RotateSpeed = 4.0f;
-    public bool LookAtPlayer = false;
-
-    void Start()
+namespace FollowPlayerNamespace
+{
+    public class FollowPlayer : MonoBehaviour
     {
-        Offset = transform.position - Player.position;
+
+        public Transform Player;
+        private Vector3 Offset;
+
+        public bool RotateCamera = true;
+        public float RotateSpeed = 4.0f;
+        public bool LookAtPlayer = false;
+
+        void Start()
+        {
+            Offset = transform.position - Player.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            Quaternion cameraAngle =
+                Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotateSpeed, Vector3.up);
+            Offset = cameraAngle * Offset;
+
+            transform.position = Player.position + Offset;
+            transform.localRotation = Player.localRotation;
+            transform.LookAt(Player);
+        }
+
     }
-
-    // Update is called once per frame
-    void Update () {
-
-        Quaternion cameraAngle = 
-            Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotateSpeed, Vector3.up);
-        Offset = cameraAngle * Offset;
-
-        transform.position = Player.position + Offset;
-        transform.localRotation = Player.localRotation;
-        transform.LookAt(Player);
-	}
-
 }
+
+
