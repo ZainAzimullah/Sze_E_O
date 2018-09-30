9e6c2aa6efa9e0a1027124020dadf262104b9be4
Brian Choi <sukhoonchoi1@gmail.com>
2018-09-30 16:47:39 +1300

    Add EXP bar sprite

---
+++ Assets/Scripts/UI Scripts/EXPBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EXPBar : MonoBehaviour {
+
+	[SerializeField]
+	private float fillAmount;
+
+	[SerializeField]
+	private Image content;
+
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		HandleBar();
+	}
+
+	private void HandleBar ()
+	{
+		content.fillAmount = Map(15, 0, 100, 0, 1);	
+	}
+
+	private float Map (float value, float inputMin, float inputMax, float outputMin, float outputMax) 
+	{
+		return (value - inputMin) * (outputMax - outputMin) / (inputMax - inputMin) + outputMin;
+	}
+}
