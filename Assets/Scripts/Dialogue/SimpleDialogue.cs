7594dad69a1409e8da5066dda1bd3cbe875676b4
Zain's University Laptop <zain.zain51@gmail.com>
2018-10-10 22:47:07 +1300

    skip button

--- Assets/Scripts/Dialogue/SimpleDialogue.cs
+++ Assets/Scripts/Dialogue/SimpleDialogue.cs
@@ -79,16 +79,22 @@ public class SimpleDialogue : MonoBehaviour {
 
     public virtual void Finish()
     {
-        // THIS IS CALLED WHEN THE DIALOGUE IS FINISHED
+        // TODO:  Refactor this!!!!!!
         if (LevelManager.Instance.currentLevel == 0)
         {
             SceneTransitionManager.Instance.LoadScene(SceneEnum.LEVEL0);
         }else if(LevelManager.Instance.currentLevel == 1)
         {
             SceneTransitionManager.Instance.LoadScene(SceneEnum.LEVEL1);
+        } else if (LevelManager.Instance.currentLevel == 2)
+        {
+            SceneTransitionManager.Instance.LoadScene(SceneEnum.LEVEL2);
         }
+    }
 
-        
+    public void Skip()
+    {
+        Finish();
     }
 
     public void BrightenMainCharacter()
