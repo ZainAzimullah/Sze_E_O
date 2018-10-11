76ee82c96aa7e16632420ff9fb61ebeaa4af8795
Chen <czha959@aucklanduni.ac.nz>
2018-10-12 00:52:29 +1300

    Make sure the panel works properly

---
+++ Assets/Scripts/Main Menu Scripts/LevelUIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUIManager : MonoBehaviour {
+    public GameObject dialogPanel;
+
+	// Use this for initialization
+	void Start () {
+        dialogPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            dialogPanel.SetActive(true);
+        }
+	}
+
+    public void OnResumeButtonClicked()
+    {
+        dialogPanel.SetActive(false);
+    }
+
+    public void OnSaveButtonClicked()
+    {
+        dialogPanel.SetActive(false);
+        //To Do: need to implement saving mechanism here
+    }
+}
