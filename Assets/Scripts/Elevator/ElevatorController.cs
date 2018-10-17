a09478001a01eca78a5a573730a6b51deb1e4d27
Zain's University Laptop <zain.zain51@gmail.com>
2018-10-17 14:19:30 +1300

    fixed elevator button highlighting

--- Assets/Scripts/Elevator/ElevatorController.cs
+++ Assets/Scripts/Elevator/ElevatorController.cs
@@ -2,18 +2,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ElevatorController : MonoBehaviour {
 
 
     public GameObject PopupPanel;
+    public Button btnG;
+    public Button btn1;
+    public Button btn2;
+    public Button btn3;
+    public Button btn4;
 
 
     // Use this for initialization
     void Start()
     {
         PopupPanel.SetActive(false);
+        Color newColor = new Color((2f/255f), (208f/255f), (255f/255f));
+        switch (LevelManager.Instance.currentLevel)
+        {
+            case 0:
+                ColorBlock colors0 = btnG.colors;
+                colors0.normalColor = newColor;
+                btnG.colors = colors0;
+                break;
+            case 1:
+                ColorBlock colors1 = btn1.colors;
+                colors1.normalColor = newColor;
+                btn1.colors = colors1;
+                break;
+            case 2:
+                ColorBlock colors2 = btn2.colors;
+                colors2.normalColor = newColor;
+                btn2.colors = colors2;
+                break;
+            case 3:
+                ColorBlock colors3 = btn3.colors;
+                colors3.normalColor = newColor;
+                btn3.colors = colors3;
+                break;
+            case 4:
+                ColorBlock colors4 = btn4.colors;
+                colors4.normalColor = newColor;
+                btn4.colors = colors4;
+                break;
+        }
     }
 
     public void GroundButton()
